fd868e2b891a9898b59455cbc8ad362b1fc24f2f
jciepka <jakub.ciepka@gmail.com>
2020-01-04 15:25:48 +0100

    implemented first NN and trained it on CIFAR10 dataset with accuracy 54%
    organized code

--- playground/playground.py
+++ playground/playground.py
@@ -19,9 +19,9 @@ x = x.new_ones(5, 3, dtype=torch.double)
 # print(x)
 
 x = torch.randn_like(x, dtype=torch.float)
-#print(x)
+print(x)
 
-#print(x.size())
+print(x.size())
 
 y = torch.rand(5, 3)
 # print(x + y)
